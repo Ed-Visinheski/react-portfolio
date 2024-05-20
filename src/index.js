6f7707a59f1c4c0987e05e11881ff1db6ec14ed5
Ed-Visinheski <eduardo.cook-visinheski@city.ac.uk>
2024-05-20 15:35:45 +0100

    changed location of root

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import reportWebVitals from '../src/reportWebVitals'
+import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
-import App from '../src/App'
-import '../src/index.css'
+import App from './App'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
